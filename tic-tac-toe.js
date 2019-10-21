562c111b07c63fe1d4a3a702e1bd53c0ee854071
Eversley Francis <everboy15@hotmail.com>
2019-10-21 18:30:40 -0500

    Added X and O to square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,38 @@
 let addBoardClass= () => {
     let entireBoard= document.getElementById('board');
-    let gridElements=entireBoard.children;
+    let cells=entireBoard.children; //Divide the board into its 9 divs
 
-    for (i=0; i< gridElements.length; i++){
-        gridElements[i]= gridElements[i].classList.add('square');
+    //Add square class to all divs
+    for (i=0; i< cells.length; i++){
+        cells[i].classList.add('square'); 
     }
+    //Add event listeners to all divs
+    for (let i=0; i<cells.length; i++){
+        cells[i].addEventListener("click",function(){
+
+            if(gameState.length==0){ //Game just started
+                cells[i].classList.add("X");
+                cells[i].innerHTML=("X");
+                gameState.push("X");
+            }else{
+                if (gameState[gameState.length - 1]== 'X'){
+                    cells[i].classList.add("O");
+                    cells[i].innerHTML=("O");
+                    gameState.push("O");
+                }else{
+                    cells[i].classList.add("X");
+                    cells[i].innerHTML=("X");
+                    gameState.push("X");
+                }
+            }
+        });
+    }
+
+
 }
 
 document.addEventListener('DOMContentLoaded', addBoardClass);
+
+var gameState=[]; 
+//Dont forget to clear this when new game starts
+
