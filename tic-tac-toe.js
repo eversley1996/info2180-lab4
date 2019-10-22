0af69834d06554278cc8d4fe6c929478d9471f50
Eversley Francis <everboy15@hotmail.com>
2019-10-21 21:14:46 -0500

    Added function to check who the winner is

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -19,17 +19,20 @@ let addBoardClass= () => {
 
             if(gameState.length==0){ //Game just started
                 cells[i].classList.add("X");
-                cells[i].innerHTML=("X");
+                cells[i].textContent=("X");
                 gameState.push("X");
+                checkWinner(cells);
             }else{
                 if (gameState[gameState.length - 1]== 'X'){
                     cells[i].classList.add("O");
-                    cells[i].innerHTML=("O");
+                    cells[i].textContent=("O");
                     gameState.push("O");
+                    checkWinner(cells);
                 }else{
                     cells[i].classList.add("X");
-                    cells[i].innerHTML=("X");
+                    cells[i].textContent=("X");
                     gameState.push("X");
+                    checkWinner(cells);
                 }
             }
         });
@@ -43,3 +46,53 @@ document.addEventListener('DOMContentLoaded', addBoardClass);
 var gameState=[]; 
 //Dont forget to clear this when new game starts
 
+/*
+    Index numbers
+
+      0  |  1   | 2
+    .................
+      3 |   4   | 5
+    .................
+      6 |   7   | 8
+    
+*/
+
+
+var checkWinner= (cells) => {
+
+    //Checks the different ways someone can win
+    if (cells[0].textContent !== "" && cells[0].textContent === cells[1].textContent && cells[0].textContent === cells[2].textContent){
+        document.getElementById('status').classList.add("you-won");
+        document.getElementById('status').textContent=   "Congratulations! "+ cells[0].textContent + " is the Winner!"
+     }
+     else if (cells[3].textContent !== "" && cells[3].textContent === cells[4].textContent && cells[3].textContent === cells[5].textContent){
+        document.getElementById('status').classList.add("you-won");
+        document.getElementById('status').textContent = "Congratulations! "+ cells[3].textContent + " is the Winner!"
+     }
+     else if (cells[6].textContent !== "" && cells[6].textContent === cells[7].textContent && cells[6].textContent === cells[8].textContent){
+        document.getElementById('status').classList.add("you-won");
+        document.getElementById('status').textContent = "Congratulations! "+ cells[6].textContent + " is the Winner!"
+     }
+     else if (cells[0].textContent !== "" && cells[0].textContent === cells[3].textContent && cells[0].textContent === cells[6].textContent){
+        document.getElementById('status').classList.add("you-won");
+        document.getElementById('status').textContent = "Congratulations! "+ cells[0].textContent + " is the Winner!"
+     }
+     else if (cells[1].textContent !== "" && cells[1].textContent === cells[4].textContent && cells[1].textContent === cells[7].textContent){
+        document.getElementById('status').classList.add("you-won");
+        document.getElementById('status').textContent = "Congratulations! "+ cells[1].textContent + " is the Winner!"
+
+     }else if (cells[2].textContent !== "" && cells[2].textContent === cells[5].textContent && cells[2].textContent === cells[8].textContent){
+        document.getElementById('status').classList.add("you-won");
+        document.getElementById('status').textContent = "Congratulations! "+ cells[2].textContent + " is the Winner!"
+
+     }else if (cells[6].textContent !== "" && cells[6].textContent === cells[4].textContent && cells[6].textContent === cells[2].textContent){
+        document.getElementById('status').classList.add("you-won");
+        document.getElementById('status').textContent =  "Congratulations! "+ cells[6].textContent + " is the Winner!"
+     }
+     else if (cells[0].textContent !== "" && cells[0].textContent === cells[4].textContent && cells[0].textContent === cells[8].textContent){
+        document.getElementById('status').classList.add("you-won");
+        document.getElementById('status').textContent = "Congratulations! "+ cells[0].textContent + " is the Winner!"
+     }
+     
+     
+}
